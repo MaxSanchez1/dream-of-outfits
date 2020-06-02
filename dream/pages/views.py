092d7c01b98d46e1001fce06f91d9ab61ceca392
MaxSanchez1 <maxwsanchez@gmail.com>
2020-06-02 13:42:13 -0400

    initial commit containing apps and setup files

---
+++ dream/pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    # return render(request, 'home.html', {})
+    return render(request, "home.html", {})
